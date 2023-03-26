31f589199f9b56150e7e09c59c6f6550ce55c6bd
Pierre Fasce <lemonpiero@gmail.com>
2023-03-26 19:32:59 +0200

    ajout composant WelcomeSection

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,17 +1,18 @@
+import myPhoto from '../../assets/pictures/moi2-min.png'
+
 function WelcomeSection() {
     return (
         <section id="welcome" className="section welcome">
             <div className="welcome__inset">
-                <img src="../../assets/pictures/moi2-min.png" alt="moi" className="welcome__inset__image"/>
+                <img src={ myPhoto } alt="moi" className="welcome__inset__image"/>
             </div>
 
             <div className="welcome__content">
                 <h2 className="welcome__content__title">Bienvenue</h2>
                 <p className="welcome__content__text">
-                    Je me présente, je m'appelle Pierre, après avoir passé 20 ans en tant qu'assistant de vente, j'ai décidé de me reconvertir dans le développement web.
-                    Ce site a été dans le but de vous présenter mon travail à travers les différents projets que j'ai réalisés.
-                    Vous y trouverez également mon parcours professionnel ainsi qu'un formulaire de contact.
-
+                    Je me présente, je m'appelle Pierre, après avoir passé 20 ans en tant qu'assistant de vente, j'ai décidé de me reconvertir dans le développement web.<br/>
+                    Ce site a été créé dans le but de vous présenter mon travail à travers les différents projets que j'ai réalisés.<br/>
+                    Vous y trouverez également mon parcours professionnel ainsi qu'un formulaire de contact.<br/><br/>
                     Bonne visite !
                 </p>
             </div>
