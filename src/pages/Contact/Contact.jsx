4474e0cda0afa14d8070ed14d8bbd600f2f6bb31
Pierre Fasce <lemonpiero@gmail.com>
2023-04-03 20:09:41 +0200

    ajout page contact ajout media queries pages home et contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,46 @@
 function Contact() {
     return (
-        <div id='contact' className='contact'>
-            <p>Page de contact</p>
-        </div> 
+        <main id="contact" className="main">
+            <section className="section">
+                <div className="section__inset">
+                    <h2 className="section__title">Contact</h2>
+                </div>
+
+                <form action="" className="form">
+                    <div className="form__div">
+                        <div className="form__item">
+                            <label htmlFor="lastname" className="form__label">Nom*</label>
+                            <input type="text" name="lastname" id="lastname" className="form__input" required/>
+                        </div>
+                        <div className="form__item">
+                            <label htmlFor="firstname" className="form__label">Prénom*</label>
+                            <input type="text" name="firstname" id="firstname" className="form__input" required/>
+                        </div>
+                        <div className="form__item">
+                            <label htmlFor="email" className="form__label">Email*</label>
+                            <input type="email" name="email" id="email" className="form__input" required/>
+                        </div>
+                        <div className="form__item">
+                            <label htmlFor="phone" className="form__label">Téléphone</label>
+                            <input type="tel" name="phone" id="phone" className="form__input"/>
+                        </div>
+                    </div>
+                    
+                    <div className="form__div">
+                        <div className="form__item">
+                            <label htmlFor="subject" className="form__label">Objet du message*</label>
+                            <input type="text" name="subject" id="subject" className="form__input" required/>
+                        </div>
+                        <div className="form__item">
+                            <label htmlFor="message">Message*</label>
+                            <textarea name="message" id="message" cols="30" rows="10" className="form__input" required></textarea>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="form__btn">Envoyer</button>
+                </form>
+            </section>
+        </main>
     )
 }
 
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,8 +1,12 @@
 function Projects() {
     return (
-        <>
-            <p>Page des projects</p>
-        </>
+        <main id="projects" className="main">
+            <section className="section">
+                <div className="section__inset">
+                    <h2 className="section__tile">Projets</h2>
+                </div>
+            </section>
+        </main>
     )
 }
 
