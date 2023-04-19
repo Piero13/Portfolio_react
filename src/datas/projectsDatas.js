037306fe5f4c09f958fc1fb0f6762e9fbbe02199
Pierre Fasce <lemonpiero@gmail.com>
2023-04-19 19:31:49 +0200

    ajout d'assets et création des datas projets

---
+++ src/datas/projectsDatas.js
@@ -0,0 +1,134 @@
+export const projectsDatas = [
+    {
+        "title": "Booki",
+        "titleDescription": "Création d'un prototype de site web pour un site de planification de vacances",
+        "projectDescription": "Intégration des maquettes de design en HTML et CSS pur en version desktop, tablette et mobile.",
+        "website": {
+            "address": "https://piero13.github.io/P2_booki/",
+            "title": "Site Web Booki"
+        },
+        "github": {
+            "address": "https://github.com/Piero13/P2_booki.git",
+            "title": "Repository GitHub Booki"
+        },
+        "languages": [
+            "faHTML5Logo", "faAlternateCSS3Logo"
+        ],
+        "tools": [
+            "../assets/tools_store/vscode.png", "../assets/tools_store/github.png"
+        ],
+        "captions": [
+            "../assets/viewer_store/Booki-1-min.png", "../assets/viewer_store/Booki-2-min.png"
+        ]
+    },
+    {
+        "title": "Oh My Food",
+        "titleDescription": "Implémentation d'un site de foodtech avec des animations CSS",
+        "projectDescription": "Intégration des maquettes de design en HTML et CSS pur en version desktop, tablette et mobile. Mise en place de la structure de navigation du site web. Ajout d'animations CSS avec Sass.",
+        "website": {
+            "address": "https://piero13.github.io/P3_oh-my-food/",
+            "title": "Site Web Oh My Food"
+        },
+        "github": {
+            "address": "https://github.com/Piero13/P3_oh-my-food.git",
+            "title": "Repository GitHub Oh My Food"
+        },
+        "languages": [
+            "faHTML5Logo", "faAlternateCSS3Logo", "faSass"
+        ],
+        "tools": [
+            "../assets/tools_store/vscode.png", "../assets/tools_store/github.png"
+        ],
+        "captions": [
+            "../assets/viewer_store/OhMyFood-1-min.png", "../assets/viewer_store/OhMyFood-2-min.png", "../assets/viewer_store/OhMyFood-3-min.png", "../assets/viewer_store/OhMyFood-4-min.png", "../assets/viewer_store/OhMyFood-5-min.png"
+        ]
+    },
+    {
+        "title": "La Chouette Agence",
+        "titleDescription": "Analyse et optimisation d'un site de web design",
+        "projectDescription": "Recherche des bonnes pratiques en développement web, implémentation d'un code HTML et CSS maintenable. Assurer l'accessibilité du site web. Optimisation de la taille et la vitesse du site, amélioration de son référencement.",
+        "website": {
+            "address": "https://piero13.github.io/P4_la_chouette_agence/",
+            "title": "Site web de La Chouette Agence"
+        },
+        "github": {
+            "address": "https://github.com/Piero13/P4_la_chouette_agence.git",
+            "title": "Repository GitHub de La Chouette Agence"
+        },
+        "languages": [
+            "faHTML5Logo", "faAlternateCSS3Logo", "faSass"
+        ],
+        "tools": [
+            "../assets/tools_store/vscode.png", "../assets/tools_store/github.png", "../assets/tools_store/lighthouse.png", "../assets/tools_store/wave.png", "../assets/tools_store/colour_contrast_analyser.png", "../assets/tools_store/optimizilla.png"
+        ],
+        "captions": [
+            "../assets/viewer_store/LaChouetteAgence-1-min.png", "../assets/viewer_store/LaChouetteAgence-2-min.png", "../assets/viewer_store/LaChouetteAgence-3-min.png", "../assets/viewer_store/LaChouetteAgence-4-min.png", "../assets/viewer_store/LaChouetteAgence-5-min.png", "../assets/viewer_store/LaChouetteAgence-6-min.png", "../assets/viewer_store/LaChouetteAgence-7-min.png", "../assets/viewer_store/LaChouetteAgence-8-min.png"
+        ]
+    },
+    {
+        "title": "Kanap",
+        "titleDescription": "Création du MVP d'un site de e-commerce",
+        "projectDescription": "Gestion des évènements Javascript. Validation des données issues de source externe. Interaction avec l'API du site avec Javascript. Création du plan de test du site.",
+        "website": {
+            "address": null,
+            "title": null
+        },
+        "github": {
+            "address": "https://github.com/Piero13/P5_kanap.git",
+            "title": "Repository GitHub de Kanap"
+        },
+        "languages": [
+            "faHTML5Logo", "faAlternateCSS3Logo", "faSass", "faJavaScript"
+        ],
+        "tools": [
+            "../assets/tools_store/vscode.png", "../assets/tools_store/github.png"
+        ],
+        "captions": [
+            "../assets/viewer_store/Kanap-1-min.png", "../assets/viewer_store/Kanap-2-min.png", "../assets/viewer_store/Kanap-3-min.png", "../assets/viewer_store/Kanap-4-min.png", "../assets/viewer_store/Kanap-5-min.png", "../assets/viewer_store/Kanap-6-min.png", "../assets/viewer_store/Kanap-7-min.png"
+        ]
+    },
+    {
+        "title": "Hot Takes",
+        "titleDescription": "Création du backend et de l'API d'une application d'avis gastronomiques de sauces piquantes",
+        "projectDescription": "Implémentation du backend et de l'API su site avec Node et Express. Gestion de la base de données NoSQL avec MongoDB. Respect des normes de l'OWASP pour la sécurisation des données.",
+        "website": {
+            "address": null,
+            "title": null
+        },
+        "github": {
+            "address": "https://github.com/Piero13/P6_piiquante.git",
+            "title": "Repository GitHub de Hot Takes"
+        },
+        "languages": [
+            "faHTML5Logo", "faAlternateCSS3Logo", "faSass", "faJavaScript", "faNode"
+        ],
+        "tools": [
+            "../assets/tools_store/vscode.png", "../assets/tools_store/github.png", "../assets/tools_store/MongoDB.png", "../assets/tools_store/Owasp.png"
+        ],
+        "captions": [
+            "../assets/viewer_store/Hot_Takes-1-min.png", "../assets/viewer_store/Hot_Takes-2-min.png", "../assets/viewer_store/Hot_Takes-3-min.png", "../assets/viewer_store/Hot_Takes-4-min.png"
+        ]
+    },
+    {
+        "title": "Groupomania",
+        "titleDescription": "Création d'un réseau social d'entrerise et manipulation des bases de données SQL",
+        "projectDescription": "Projet Fullstack. Authentification d'un utilisateur et maintien de sa session. Implémentation et gestion d'un stockage de données sécurisé à l'aide de SQL. Personnalisation d'un contenu envoyé à un client web. Utilisation d'un framework JavaScript",
+        "website": {
+            "address": null,
+            "title": null
+        },
+        "github": {
+            "address": "https://github.com/Piero13/P7_Groupomania.git",
+            "title": "Repository GitHub de Groupomania"
+        },
+        "languages": [
+            "faHTML5Logo", "faAlternateCSS3Logo", "faSass", "faVue.js"
+        ],
+        "tools": [
+            "../assets/tools_store/vscode.png", "../assets/tools_store/github.png", "../assets/tools_store/Owasp.png", "../assets/tools_store/mysql.png"
+        ],
+        "captions": [
+            "../assets/viewer_store/groupomania-1-min.png", "../assets/viewer_store/groupomania-2-min.png", "../assets/viewer_store/groupomania-3-min.png", "../assets/viewer_store/groupomania-4-min.png", "../assets/viewer_store/groupomania-5-min.png", "../assets/viewer_store/groupomania-6-min.png", "../assets/viewer_store/groupomania-7-min.png", "../assets/viewer_store/groupomania-8-min.png", 
+        ]
+    },
+]
